10c239866ce77dae4f4e1f95654089bec89e474a
laveberry <laveberry7@gmail.com>
2023-08-28 19:41:59 +0900

    컨버전 서비스 - conversionService

---
+++ src/test/java/laveberry/typeconverter/convert/ConversionServiceTest.java
@@ -0,0 +1,31 @@
+package laveberry.typeconverter.convert;
+
+import laveberry.typeconverter.converter.IntegerToStringConverter;
+import laveberry.typeconverter.converter.IpPortToStringConverter;
+import laveberry.typeconverter.converter.StringToIntegerConverter;
+import laveberry.typeconverter.converter.StringToIpPortConverter;
+import laveberry.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService() {
+        //등록
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        //사용
+        assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+        assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+        assertThat(conversionService.convert("127.0.0.1:8080", IpPort.class)).isEqualTo(new IpPort("127.0.0.1", 8080));
+        assertThat(conversionService.convert(new IpPort("127.0.0.1", 8080), String.class)).isEqualTo("127.0.0.1:8080");
+    }
+}
